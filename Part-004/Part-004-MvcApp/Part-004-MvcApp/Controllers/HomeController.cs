759d143dc0e2dcc90a0b6644e3fbc7175916edf2
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2015-11-08 02:17:43 +0530

    Part-004

---
+++ Part-004/Part-004-MvcApp/Part-004-MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Part_004_MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {        
+        public String Index(String id, String name)
+        {
+            // Using queristring
+            //return "Hello from MVC App, id = <b>" + id + "</b>, Name = <b>" + Request.QueryString["name"] + "</b>";
+
+            return "Hello from MVC App, id = <b>" + id + "</b>, Name = <b>" + name + "</b>";
+        }
+
+    }
+}

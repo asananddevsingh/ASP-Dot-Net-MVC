5bbd8fe79b8e8b7901cd33220b8b808d9bd891e6
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2015-11-08 21:11:31 +0530

    Part 006 ViewData and ViewBag in mvc

---
+++ Part-006/Part-006-ViewDataAndViewBag/Part-006-ViewDataAndViewBag/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Part_006_ViewDataAndViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewData["countries"] = new List<String>()
+            {
+                "India",
+                "US",
+                "Australia"
+            };
+            return View();
+        }
+
+    }
+}
---
+++ Part-006/Part-006-ViewDataAndViewBag/Part-006-ViewDataAndViewBag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Part_006_ViewDataAndViewBag.MvcApplication" Language="C#" %>

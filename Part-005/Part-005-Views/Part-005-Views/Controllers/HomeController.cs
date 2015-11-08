3842eef0aa14876e233138372cb72d45a652fd4e
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2015-11-08 21:01:06 +0530

    Part 005 Views in an mvc application

---
+++ Part-005/Part-005-Views/Part-005-Views/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Part_005_Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            /* This will print the type of the list insted of list of countries. ( return type of function should be List<String>) */
+            //return new List<string> () 
+            //{  
+            //    "India", 
+            //    "US",
+            //    "UK",
+            //    "Canada"
+            //};
+
+            ViewBag.Countries = new List<string>() 
+            {  
+                "India",
+                "US",
+                "UK",
+                "Canada"
+            };
+            return View();
+                 
+        }
+
+    }
+}
